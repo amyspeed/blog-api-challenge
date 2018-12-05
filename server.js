4d2f88f24bb7ba059216d4ec16b0f1f6480d57f4
Amy Speed <amy.speed.henley@gmail.com>
2018-12-04 18:21:34 -0600

    Get complete

--- server.js
+++ server.js
@@ -3,22 +3,22 @@ const router = express.Router();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
-const {BlogPosts} = require('./models');
+const {BlogPosts} = require('./model');
 
-const jsonParser = BodyParser.json();
+const jsonParser = bodyParser.json();
 const app = express();
 
 app.use(morgan('common'));
 
 BlogPosts.create(
-    'How to Express your Inner Unicorn', 'This is the content. This is more content. Here is even more content!!', 'Crystal Magicson', Date.now()
+    'How to Express your Inner Unicorn', 'This is the content. This is more content. Here is even more content!!', 'Crystal Magicson', new Date('December 1, 2018 03:24:00')
     );
 BlogPosts.create(
-    'The Day the Internet Died', 'Bye bye to the clouds in the sky. Drove my server even further and the wifi was dry.', 'Digital McLean', Date.now()
+    'The Day the Internet Died', 'Bye bye to the clouds in the sky. Drove my server even further and the wifi was dry.', 'Digital McLean', new Date('November 1, 2018 03:24:00')
 );
 
 app.get('/blog-posts', (req, res) => {
-    res.json(ShoppingList.get());
+    res.json(BlogPosts.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
