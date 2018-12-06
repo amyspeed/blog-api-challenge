38d55b8c71aa0c8ad0250e89d9bc5b78125ffbf2
Amy Speed <amy.speed.henley@gmail.com>
2018-12-05 20:56:56 -0600

    post script

--- server.js
+++ server.js
@@ -21,6 +21,20 @@ app.get('/blog-posts', (req, res) => {
     res.json(BlogPosts.get());
 });
 
+app.post('/blog-posts', jsonParser, (req, res) => {
+    const requiredFields = ['name', 'content', 'author'];
+    for (let i=0; i<requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+    const post = BlogPosts.create(req.body.name, req.body.content, req.body.author);
+    res.status(201).json(post);
+});
+
 app.listen(process.env.PORT || 8080, () => {
     console.log(`your app is listening on port ${process.env.PORT || 8080}`);
 });
